319e8d1c2e5027de16e84863a36a8a44bbe712a4
BZIvanov <ivanov.biser@gmx.com>
2021-01-30 23:45:34 +0200

    create api gateway function example

---
+++ demos/SDK-APIgateway-api/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json({ limit: '10kb' }));
+app.use('/', routes);
+
+const PORT = 3100;
+app.listen(PORT, function () {
+  console.log(
+    `AWS APIGateway example app listening at http://localhost:${PORT}`
+  );
+});
