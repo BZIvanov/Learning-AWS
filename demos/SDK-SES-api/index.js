e764de64e407f38f7e0891fec9be269e7b0708f8
BZIvanov <biser.z.ivanov@gmail.com>
2021-02-01 22:24:58 +0200

    sdk ses api examples

---
+++ demos/SDK-SES-api/index.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+app.use(express.json({ limit: '10kb' }));
+app.use('/', routes);
+
+const PORT = 3100;
+app.listen(PORT, function () {
+  console.log(`AWS SES example app listening at http://localhost:${PORT}`);
+});
