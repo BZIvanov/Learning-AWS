3480783b6191fc9ba06b023e83d246266990c8c1
BZIvanov <ivanov.biser@gmx.com>
2021-02-01 23:02:54 +0200

    sdk ses examples

--- demos/SDK-SES-api/routes/index.js
+++ demos/SDK-SES-api/routes/index.js
@@ -4,11 +4,17 @@ const {
   sendEmail,
   createTemplate,
   sendTemplatedEmail,
+  getSendStatistics,
+  getAccountSendingEnabled,
+  deleteTemplate,
 } = require('../controllers');
 
 router.post('/verifyEmailIdentity', verifyEmailIdentity);
 router.post('/sendEmail', sendEmail);
 router.post('/createTemplate', createTemplate);
 router.post('/sendTemplatedEmail', sendTemplatedEmail);
+router.get('/getSendStatistics', getSendStatistics);
+router.get('/getAccountSendingEnabled', getAccountSendingEnabled);
+router.post('/deleteTemplate', deleteTemplate);
 
 module.exports = router;
