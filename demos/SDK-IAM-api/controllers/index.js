1d9ad3b624078589deccbd1306315a6b85fbada7
BZIvanov <biser.z.ivanov@gmail.com>
2021-02-03 23:08:22 +0200

    more iam api examples

--- demos/SDK-IAM-api/controllers/index.js
+++ demos/SDK-IAM-api/controllers/index.js
@@ -7,7 +7,7 @@ const config = new AWS.Config({
   apiVersion: '2010-05-08',
 });
 
-const ses = new AWS.IAM(config);
+const iam = new AWS.IAM(config);
 
 exports.createGroup = async (req, res) => {
   const { name } = req.body;
@@ -16,7 +16,7 @@ exports.createGroup = async (req, res) => {
     GroupName: name,
   };
   try {
-    const result = await ses.createGroup(params).promise();
+    const result = await iam.createGroup(params).promise();
     res.send(result);
   } catch (err) {
     res.send(err);
@@ -30,7 +30,175 @@ exports.createUser = async (req, res) => {
     UserName: name,
   };
   try {
-    const result = await ses.createUser(params).promise();
+    const result = await iam.createUser(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.addUserToGroup = async (req, res) => {
+  const { groupName, userName } = req.body;
+
+  const params = {
+    GroupName: groupName,
+    UserName: userName,
+  };
+  try {
+    const result = await iam.addUserToGroup(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.getGroup = async (req, res) => {
+  const { name } = req.body;
+
+  const params = {
+    GroupName: name,
+  };
+  try {
+    const result = await iam.getGroup(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.createRole = async (req, res) => {
+  const { name } = req.body;
+
+  const params = {
+    RoleName: name,
+    Description: 'My role description.',
+    AssumeRolePolicyDocument: JSON.stringify({
+      Version: '2012-10-17',
+      Statement: {
+        Effect: 'Allow',
+        Principal: { Service: 's3.amazonaws.com' }, // this is our trusted entity, which can assume this role
+        Action: 'sts:AssumeRole',
+      },
+    }),
+  };
+  try {
+    const result = await iam.createRole(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.createPolicy = async (req, res) => {
+  const { name } = req.body;
+
+  const params = {
+    PolicyName: name,
+    Description: 'My policy description.',
+    PolicyDocument: JSON.stringify({
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: 's3:PutObject',
+          Resource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+        {
+          Effect: 'Deny',
+          NotAction: 's3:PutObject', // deny anything but this action
+          Resource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+        {
+          Effect: 'Deny',
+          Action: 's3:*',
+          NotResource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+      ],
+    }),
+  };
+  try {
+    const result = await iam.createPolicy(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+
+  // on this link you can find more examples
+  // https://docs.aws.amazon.com/AmazonS3/latest/dev/example-policies-s3.html
+};
+
+exports.createPolicyVersion = async (req, res) => {
+  const { arn } = req.body;
+
+  const params = {
+    PolicyArn: arn,
+    SetAsDefault: true,
+    PolicyDocument: JSON.stringify({
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: ['s3:GetObject', 's3:PutObject'],
+          Resource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+        {
+          Effect: 'Deny',
+          NotAction: ['s3:GetObject', 's3:PutObject'],
+          Resource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+        {
+          Effect: 'Deny',
+          Action: 's3:*',
+          NotResource: 'arn:aws:s3:::mycorporatebucket/uploads/widgetco/*',
+        },
+      ],
+    }),
+  };
+  try {
+    const result = await iam.createPolicyVersion(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.getPolicy = async (req, res) => {
+  const { arn } = req.body;
+
+  const params = {
+    PolicyArn: arn,
+  };
+  try {
+    const result = await iam.getPolicy(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.attachGroupPolicy = async (req, res) => {
+  const { groupName, arn } = req.body;
+
+  const params = {
+    GroupName: groupName,
+    PolicyArn: arn,
+  };
+  try {
+    const result = await iam.attachGroupPolicy(params).promise();
+    res.send(result);
+  } catch (err) {
+    res.send(err);
+  }
+};
+
+exports.getUser = async (req, res) => {
+  const { name } = req.body;
+
+  const params = {
+    UserName: name,
+  };
+  try {
+    const result = await iam.getUser(params).promise();
     res.send(result);
   } catch (err) {
     res.send(err);
--- demos/SDK-IAM-api/routes/index.js
+++ demos/SDK-IAM-api/routes/index.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
-const { createGroup, createUser } = require('../controllers');
+const {
+  createGroup,
+  createUser,
+  addUserToGroup,
+  getGroup,
+  createRole,
+  createPolicy,
+  createPolicyVersion,
+  getPolicy,
+  attachGroupPolicy,
+  getUser,
+} = require('../controllers');
 
 router.post('/createGroup', createGroup);
 router.post('/createUser', createUser);
+router.post('/addUserToGroup', addUserToGroup);
+router.post('/getGroup', getGroup);
+router.post('/createRole', createRole);
+router.post('/createPolicy', createPolicy);
+router.post('/createPolicyVersion', createPolicyVersion);
+router.post('/getPolicy', getPolicy);
+router.post('/attachGroupPolicy', attachGroupPolicy);
+router.post('/getUser', getUser);
 
 module.exports = router;
